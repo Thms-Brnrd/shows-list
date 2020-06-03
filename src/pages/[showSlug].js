9bf6a146b24f69ef0b7ebeb1233652a9730e727a
Thomas Bernard <thomas@ausha.co>
2020-06-03 16:26:41 +0200

    ♻️ Add errors description

--- src/pages/[showSlug].js
+++ src/pages/[showSlug].js
@@ -7,12 +7,15 @@ const Show = ({ showSlug, isRewriting }) => {
             <h1>Show {showSlug}</h1>
             <ul>
                 <li>
+                    {/* Error href and as mismatch */}
                     <Link href="/[showSlug]/[episodeSlug]" as={isRewriting ? '/episode1' : `/${showSlug}/episode1`}>
                         <a>Episode 1</a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/[showSlug]/[episodeSlug]" as={isRewriting ? '/episode2' : `/${showSlug}/episode2`}>
+                    {/* Based on https://github.com/vercel/next.js/pull/9837/files */}
+                    {/* Episode page is used but "episode2" is used as the showSlug, not the episodeSlug */}
+                    <Link href={`/[showSlug]/[episodeSlug]?showSlug=${showSlug}&episodeSlug=episode2`} as={isRewriting ? '/episode2' : `/${showSlug}/episode2`}>
                         <a>Episode 2</a>
                     </Link>
                 </li>
